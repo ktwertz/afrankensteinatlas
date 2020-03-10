531f4f5f85c0de6d06d8c9094e43a7a371a1a815
ktwertz <61473960+ktwertz@users.noreply.github.com>
2020-03-10 09:51:21 -0400

    Update essay1.php

--- essay1.php
+++ essay1.php
@@ -6,6 +6,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+<!-- Header Section -->
+<?php include("header.php"); 
+?>
+
 <p>In the novel, Victor Frankenstein pushes the limits of science for its own sake,
 without concern for the consequences of his actions. At the same time, emerging
 European nation-states were pushing the limits of discovery and control over the Earth
@@ -14,12 +18,6 @@ to physical exploration of the globe. The book can therefore be read as a critiq
 of colonialism as an outgrowth of Europeans' insatiable desire to expand their 
 knowledge and control over the physical world. I hope to employ GIS and maps as a form of 
 multi-modal story telling to enhance this argument.</p>
-
-
-<!-- Header Section -->
-<?php include("header.php"); 
-?>
-
 
 <!-- Footer Section -->
 <?php include("footer.php"); 
