60e6a1ce3f8e03a49d500fd37a926d05cefa1348
ktwertz <61473960+ktwertz@users.noreply.github.com>
2020-03-10 09:40:19 -0400

    Update essay1.php

--- essay1.php
+++ essay1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Exploration and Colonialism in Frankenstein</title>
+<title>Exploration, Colonialism, and Science in Frankenstein</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
