fac3796c28d999575ffb7ec53122bb62fc527f6e
ktwertz <61473960+ktwertz@users.noreply.github.com>
2020-03-10 09:51:16 -0400

    Create essay2.php

---
+++ essay2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Frankenstein as Metaphor for Doping in Sports</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<!-- Header Section -->
+<?php include("header.php"); 
+?>
+
+<p> In popular culture, "Frankenstein" has taken on a meaning beyond just a character
+in a classic novel. It carries an implication of inhumanity, of science taken too far.
+This essay will explore how this word and its implications have been used in discourse
+around the use of performance-enhancing drugs in sports. In my research so far, I have 
+noticed that this word is much more often used to refer to athletes and doctors from
+Communist countries such as the USSR and East Germany, so the Cold War is also a possible 
+context I will be exploring. </p>
+
+<!-- Footer Section -->
+<?php include("footer.php"); 
+?>
+
+<!-- End page content -->
+</div>
+
+
+
+
+</body>
+</html>
+© 2020 GitHub, Inc.
