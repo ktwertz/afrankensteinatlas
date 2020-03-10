e59171b54255a23142fcb6ef999a316b88e25f8d
ktwertz <61473960+ktwertz@users.noreply.github.com>
2020-03-10 09:39:17 -0400

    Create essay1.php

---
+++ essay1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Exploration and Colonialism in Frankenstein</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<p>In the novel, Victor Frankenstein pushes the limits of science for its own sake,
+without concern for the consequences of his actions. At the same time, emerging
+European nation-states were pushing the limits of discovery and control over the Earth
+and its inhabitants. Several times, Shelley directly analogizes Frankenstein's quest 
+to physical exploration of the globe. The book can therefore be read as a critique 
+of colonialism as an outgrowth of Europeans' insatiable desire to expand their 
+knowledge and control over the physical world. I hope to employ GIS and maps as a form of 
+multi-modal story telling to enhance this argument.</p>
+
+
+<!-- Header Section -->
+<?php include("header.php"); 
+?>
+
+
+<!-- Footer Section -->
+<?php include("footer.php"); 
+?>
+
+<!-- End page content -->
+</div>
+
+
+
+
+</body>
+</html>
+© 2020 GitHub, Inc.
